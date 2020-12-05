0c7b3041f8ca6ac32e3b9bcbaf6a73fa2bc4a26c
staylazyy.web <tresh45rus@gmail.com>
2020-12-05 18:39:11 +0300

    small refactoring

--- page-objects/jivoApp.js
+++ page-objects/jivoApp.js
@@ -30,20 +30,24 @@ module.exports = {
       return this
         .setValue('@registrationEmailInput', email)
         .setValue('@registrationPasswordInput', password)
-        .assert.not.elementPresent('[data-qa-id="error"]', 'Step 2: Enter correct data');
+        .assert.not.elementPresent('[data-qa-id="error"]', 'Step 2: Enter correct data')
+        .click('@registrationEulaButton')
+        .click('@registrationContinueButton');
     },
     wizardEnterAgentInfo(name, description){
       return this
         .setValue('@wizardNameInput', name)
         .setValue('@wizardDescriptionInput', description)
-        .assert.not.attributeContains('@wizardNameInput', 'class', 'error', 'Step 4: Enter correct user name');
+        .assert.not.attributeContains('@wizardNameInput', 'class', 'error', 'Step 4: Enter correct user name')
+        .click('@wizardNextButton');
     },
     wizardEnterAgentContacts(url, phone){
       return this
         .setValue('@wizardUrlInput', url)
         .setValue('@wizardPhoneInput', phone)
         .assert.not.attributeContains('@wizardUrlInput', 'class', 'error', 'Step 5: Enter correct website url')
-        .assert.not.attributeContains('@wizardUrlInput', 'class', 'error', 'Step 6: Enter correct phone number');
+        .assert.not.attributeContains('@wizardUrlInput', 'class', 'error', 'Step 6: Enter correct phone number')
+        .click('@wizardNextButton');
     },
     
   }]
