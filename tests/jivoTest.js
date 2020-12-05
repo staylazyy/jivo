f4511203d8ee396306810995d0a57d0828734276
staylazyy.web <tresh45rus@gmail.com>
2020-12-05 16:00:44 +0300

    added test case for login with correct data

--- tests/jivoTest.js
+++ tests/jivoTest.js
@@ -1,15 +1,15 @@
 module.exports = {
-  'Case 1: Check if user can register and login'(browser) {
+  /*'Case 1: Check if user can register and login'(browser) {
 
     const jivo = browser.page.jivoApp();
     
     jivo
       .navigate()
-      .waitForElementVisible('button[data-qa-id="login-submit-button"]', 6000, 'Step 1: Open app page')
+      .waitForElementVisible('@loginButton', 6000, 'Step 1: Open app page')
       .click('@registerButton')
       .pause(3000)
-      .enterEmailAndPass('tetetets34t@mail.com', 'password!!AA123')//pass correct email and password in parametrs
-      .click('@eulaButton')
+      .registrationEmailAndPass('tetetets34t@mail.com', 'password!!AA123')//pass correct email and password in parametrs
+      .click('@registrationEulaButton')
       .click('@registrationContinueButton')
       .waitForElementVisible('@wizardNameInput', 10000, 'Step 2: Enter correct login and password and continue registration')
       .wizardEnterAgentInfo('MochitoAgent','Always drinking Mochito')//pass name of agent and description in parametrs
@@ -19,7 +19,20 @@ module.exports = {
       .click('@wizardNextButton')
       .click('@wizardDownloadLaterButton')
       .click('@wizardCompleteButton')
-      .waitForElementVisible('div[class*="container___"]', 10000, 'Step 3: Complete setup-wizard');
-    //sql-command example to check?
+      .waitForElementVisible('div[class*="container___"]', 10000, 'Step 3: Complete setup-wizard')
+      //sql-command example to check?
+      .end();
+  },*/
+  'Case 2: Check if user can login with correct credits'(browser) {
+
+    const jivo = browser.page.jivoApp();
+
+    jivo
+      .navigate()
+      .waitForElementVisible('@loginButton', 6000, 'Step 1: Open app page')
+      .loginEmailAndPass('oooooo1111@mail.ru', '223791aaA')
+      .click('@loginButton')
+      .pause(5000)
+      .waitForElementVisible('[data-qa-id="side-menu-item-settings"]', 6000, 'Step 2: Enter correct data and login');
   }
 };
